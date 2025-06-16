085c40bac3ed63de4ac508c599fdb9da85674999
yoshi <yoshitarazdan74@gmail.com>
2025-06-16 16:48:52 +0530

    Character Generator
    Uses subroutines, while loop and if statements, basic math functions AND COLOR CODE!
    Asks you to name your character and then when subroutine is called out picks 2 random numbers uses some operations on them and then gives the value as the character's health score. 
    Also, it uses color codes too! If the health is below 2/3rd of the max health possible, it shows health in red color, and if the health is above or equal to 32, it goes green color. Anything between 16 and 32 is color coded yellow. It's random, it's fun.

---
+++ day21_to_day40/day25.py
@@ -0,0 +1,25 @@
+print()
+print("\033[35m""CHARACTER STAT GENERATOR""\033[0m")
+print()
+while True:
+    print()
+    name = input("Name your warrior: ")
+    def rolldice(size):
+        import random
+        pong = ""
+        pong = random.randint(1, 6)
+        return pong
+    justdoit = rolldice(1)
+    def rolldice2(size):
+        import random
+        pong2 = ""
+        pong2 = random.randint(1, 8)
+        return pong2
+    justdoit2 = rolldice2(1)
+
+    if justdoit*justdoit2 <= 16:
+        print("Their health is" "\033[31m" ,justdoit * justdoit2 , "\033[0m" "hp")
+    elif justdoit*justdoit2 >= 32:
+        print("Their health is " "\033[32m", justdoit * justdoit2, "\033[0m" "hp")
+    else:
+        print("Their health is" "\033[33m" , justdoit * justdoit2, "\033[0m" "hp")
