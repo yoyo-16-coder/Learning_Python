24585ee738775112f6657e984483e7857b9f4bda
yoshi <yoshitarazdan74@gmail.com>
2025-06-16 18:56:33 +0530

    Add files via upload

---
+++ day14_to_day20/day16/day16.py
@@ -0,0 +1,14 @@
+print()
+print("FILL IN THE BLANKS LYRICS")
+print("type in the lyrics, see if you're as cool as me")
+print()
+print()
+count = 0
+while True:
+    i = input("My name is _______")
+    count = count + 1
+    if i == "thunder":
+        break
+    else :
+        print("Nope try again")
+print("CONGRATS! You got it in" ,count, "try/tries!")
