9b4a0e04de0057ec522ae58698c3823e6e820309
yoshi <yoshitarazdan74@gmail.com>
2025-06-22 01:55:49 +0530

    Create day31.py

---
+++ Replit_100_Days_Code_Challenge/day21_to_day40/Day_40_Designing_ANSI_colors/day31.py
@@ -0,0 +1,36 @@
+import time, os
+
+title=("\033[31m""=""\033[0m" "=""\033[34m""=" "\033[0m" "\033[33m"" Music App ""\033[0m""\033[31m" "=" "\033[0m""=""\033[34m" "=" "\033[0m")
+print(f"{title: ^220}")
+print("🔥▶️  Radio Gaga")
+queen = "\033[33m" "Queen""\033[0m"
+print(f"{" ": ^5}", queen)
+print()
+print("PREV")
+ing = "\033[32m""NEXT""\033[0m"
+print(f"{ing: ^23}")
+ming = "\033[35m""PAUSE""\033[0m"
+print(f"{ming: ^35}")
+
+time.sleep(5)
+os.system("cls")
+
+x = "WELCOME TO"
+print(f"{x: ^175}")
+y = "\033[34m" "--  ARMBOOK --" "\033[0m"
+print(f"{y: ^183}")
+z = "\033[33m""Definitely not a rip off of""\033[0m"
+z2 = "\033[33m""a certain other social""\033[0m"
+z3 = "\033[33m""networking site.""\033[0m"
+print(f"""
+      {z: >189}
+      {z2 : >189}
+      {z3: >189}""")
+print()
+a = "\033[31m""Honest.""\033[0m"
+print(f"{a: ^185}")
+print()
+name = "Username: "
+password = "Password:"
+print(f"{name: ^177}")
+print(f"{password: ^175}")
