f85931d0a3a5d026cf9cdfa9ce70d65c5a7a3dbf
yoshi <yoshitarazdan74@gmail.com>
2025-07-05 17:51:40 +0530

    Delete Replit_100_Days_Code_Challenge/day41_to_day60/Day45_ToDoList_Manager/test.py

--- Replit_100_Days_Code_Challenge/day41_to_day60/Day45_ToDoList_Manager/test.py
+++
@@ -1,7 +0,0 @@
-todolist = []
-row = ["bitch","idk","what im doing"]
-todolist.append(row)
-for i in range(len(row)):
-    ask = input("What do you think")
-    todolist[i][0] == ask
-    print(todolist)
